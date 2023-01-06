84e77acac57b5d200efb6539f092d58c12d2d15c
tabrez510 <alamtabrez510@gmail.com>
2023-01-06 23:21:23 +0530

    landing page responsive

--- src/components/LandingPage/Intro/index.js
+++ src/components/LandingPage/Intro/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "../../Common/Button/Button";
 import "./styles.css";
 import { motion } from "framer-motion";
+import PhoneComponent from "./PhoneComponent";
 function LandingIntro() {
   return (
     <div className={"wrapper"}>
@@ -33,15 +34,15 @@ function LandingIntro() {
         </motion.p>
         <motion.div
           className={"flexBtn"}
-          initial={{ x: 50, opacity: 0 }}
-          whileInView={{ x: 0, opacity: 1 }}
-          transition={{ duration: 0.5, delay: 0.75 }}
+          initial={{ scale: 0, opacity: 0 }}
+          whileInView={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.75 }}
         >
           <Button text="Dashboard" />
           <Button text="Share" outlined={true} />
         </motion.div>
       </div>
-      <div className={"phone"}></div>
+      <PhoneComponent />
     </div>
   );
 }
