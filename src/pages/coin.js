cc285283e822c2cfa7f15ea95a7f22d0444fb0c5
tabrez510 <alamtabrez510@gmail.com>
2023-01-12 00:02:28 +0530

    chart js

--- src/pages/coin.js
+++ src/pages/coin.js
@@ -5,37 +5,45 @@ import Info from "../components/Coin/Info/info";
 import Header from "../components/Common/Header";
 import Loading from "../components/Common/Loading/loading";
 import List from "../components/Dashboard/ListComponent/List";
+import { getCoinData } from "../functions/getCoinData";
+import { getCoinPrices } from "../functions/getCoinPrices";
 
 function CoinPage() {
   const { id } = useParams();
   const [coin, setCoin] = useState({});
+  const [days, setDays] = useState(7);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (id) {
-      axios
-        .get(`https://api.coingecko.com/api/v3/coins/${id}`)
-        .then((response) => {
-          console.log("Response Data >>>", response.data);
-          setCoin({
-            id: response.data.id,
-            name: response.data.name,
-            symbol: response.data.symbol,
-            image: response.data.image.large,
-            desc: response.data.description.en,
-            price_change_percentage_24h:
-              response.data.market_data.price_change_percentage_24h,
-            total_volume: response.data.market_data.total_volume.usd,
-            current_price: response.data.market_data.current_price.usd,
-            market_cap: response.data.market_data.market_cap.usd,
-          });
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.log("Error>>>", error);
-        });
+      getData();
     }
   }, [id]);
+
+  const getData = async () => {
+    const data = await getCoinData(id);
+    const prices = await getCoinPrices(id, days);
+
+    if (data) {
+      console.log("data", data);
+      setCoin({
+        id: data.id,
+        name: data.name,
+        symbol: data.symbol,
+        image: data.image.large,
+        desc: data.description.en,
+        price_change_percentage_24h:
+          data.market_data.price_change_percentage_24h,
+        total_volume: data.market_data.total_volume.usd,
+        current_price: data.market_data.current_price.usd,
+        market_cap: data.market_data.market_cap.usd,
+      });
+      setLoading(false);
+    }
+    if (prices) {
+      console.log("Prices", prices);
+    }
+  };
 
   return (
     <div>
