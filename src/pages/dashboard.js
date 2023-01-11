c5cfe9c09ee36233e61a70a7160b82169e6efa53
tabrez510 <alamtabrez510@gmail.com>
2023-01-11 23:06:46 +0530

    dashboard-pagination

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -6,11 +6,23 @@ import Tabs from "../components/Dashboard/Tabs/tabs";
 import { DASHBOARD_API_URL } from "../constants";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import Loading from "../components/Common/Loading/loading";
+import PaginationComponent from "../components/Dashboard/PaginationComponent/pagination";
 
 function DashboardPage() {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
   const [loading, setLoading] = useState(true);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [pageCoins, setPageCoins] = useState([]);
+
+  var filteredCoins = data.filter((item) => {
+    if (
+      item.symbol.toLowerCase().includes(search.toLowerCase()) ||
+      item.name.toLowerCase().includes(search.toLowerCase())
+    ) {
+      return item;
+    }
+  });
 
   useEffect(() => {
     axios
@@ -19,21 +31,13 @@ function DashboardPage() {
         console.log("Response Data >>>", response.data);
         setData(response.data);
         setLoading(false);
+        setPageCoins(response.data.slice(0, 10));
       })
       .catch((error) => {
         console.log("Error>>>", error);
       });
   }, []);
 
-  var filteredCoins = data.filter((item) => {
-    if (
-      item.symbol.toLowerCase().includes(search.toLowerCase()) ||
-      item.name.toLowerCase().includes(search.toLowerCase())
-      ) {
-        return item;
-      }
-    });
-  
     function topFunction() {
       document.body.scrollTop = 0;
       document.documentElement.scrollTop = 0;
@@ -56,6 +60,11 @@ function DashboardPage() {
       }
     }
 
+    const handleChange = (event, value) => {
+      setPageNumber(value);
+      setPageCoins(data.slice((value - 1) * 10, (value - 1) * 10 + 10));
+    };
+  
   return (
     <div>
       <Header />
@@ -64,10 +73,16 @@ function DashboardPage() {
       ) : (
         <>
           <Search search={search} setSearch={setSearch} />
-          <Tabs data={filteredCoins} />
+          <Tabs data={search ? filteredCoins : pageCoins} />
           <div onClick={() => topFunction()} id="myBtn" className="top-btn">
             <ArrowUpwardIcon sx={{ color: "var(--blue)" }} />
           </div>
+          {!search && (
+            <PaginationComponent
+              pageNumber={pageNumber}
+              handleChange={handleChange}
+            />
+          )}
         </>
       )}
     </div>
