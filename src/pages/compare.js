e0e3324182d75a2a6181dc0fb184de880d8d7943
tabrez510 <alamtabrez510@gmail.com>
2023-01-12 17:59:16 +0530

    2 y axises

--- src/pages/compare.js
+++ src/pages/compare.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import LineChart from "../components/Coin/Chart";
 import Info from "../components/Coin/Info/info";
 import SelectCoin from "../components/Coin/SelectCoin/selectCoin";
 import SelectDays from "../components/Coin/SelectDays/selectDays";
@@ -7,18 +8,86 @@ import Header from "../components/Common/Header";
 import Loading from "../components/Common/Loading/loading";
 import List from "../components/Dashboard/ListComponent/List";
 import { DASHBOARD_API_URL } from "../constants";
+import { convertNumbers } from "../functions/convertNumber";
 import { getCoinData } from "../functions/getCoinData";
+import { getCoinPrices } from "../functions/getCoinPrices";
+import { getDate } from "../functions/getDate";
 
 function ComparePage() {
   const [coin1, setCoin1] = useState("bitcoin");
   const [coin2, setCoin2] = useState("ethereum");
-  const [days, setDays] = useState(120);
+  const [days, setDays] = useState(90);
+  const [priceType, setPriceType] = useState("prices");
   const [allCoins, setAllCoins] = useState([]);
-
   const [loading, setLoading] = useState(true);
-
   const [coinData1, setCoinData1] = useState({});
   const [coinData2, setCoinData2] = useState({});
+
+  const [chartData, setChartData] = useState({
+    labels: [],
+    datasets: [{}],
+  });
+
+  const options = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    responsive: true,
+    interaction: {
+      mode: "index",
+      intersect: false,
+    },
+    scales: {
+      y: {
+        type: "linear",
+        display: true,
+        position: "left",
+        ticks:
+          priceType == "market_caps"
+            ? {
+                callback: function (value) {
+                  return "$" + convertNumbers(value);
+                },
+              }
+            : priceType == "total_volumes"
+            ? {
+                callback: function (value) {
+                  return convertNumbers(value);
+                },
+              }
+            : {
+                callback: function (value, index, ticks) {
+                  return "$" + value.toLocaleString();
+                },
+              },
+      },
+      y1: {
+        type: "linear",
+        display: true,
+        position: "right",
+        ticks:
+          priceType == "market_caps"
+            ? {
+                callback: function (value) {
+                  return "$" + convertNumbers(value);
+                },
+              }
+            : priceType == "total_volumes"
+            ? {
+                callback: function (value) {
+                  return convertNumbers(value);
+                },
+              }
+            : {
+                callback: function (value, index, ticks) {
+                  return "$" + value.toLocaleString();
+                },
+              },
+      },
+    },
+  };
 
   useEffect(() => {
     axios
@@ -37,8 +106,10 @@ function ComparePage() {
     const data1 = await getCoinData(coin1);
     const data2 = await getCoinData(coin2);
 
+    const prices1 = await getCoinPrices(coin1, days, priceType);
+    const prices2 = await getCoinPrices(coin2, days, priceType);
+
     if (data1) {
-      console.log("data1", data1);
       setCoinData1({
         id: data1.id,
         name: data1.name,
@@ -54,7 +125,6 @@ function ComparePage() {
     }
 
     if (data2) {
-      console.log("data2", data2);
       setCoinData2({
         id: data2.id,
         name: data2.name,
@@ -68,6 +138,36 @@ function ComparePage() {
         market_cap: data2.market_data.market_cap.usd,
       });
     }
+    if (prices1 && prices2) {
+      setChartData({
+        labels: prices1?.map((data) => getDate(data[0])),
+        datasets: [
+          {
+            label: "Coin 1",
+            data: prices1?.map((data) => data[1]),
+            borderWidth: 1,
+            fill: false,
+            tension: 0.25,
+            backgroundColor: "transparent",
+            borderColor: "#3a80e9",
+            pointRadius: 0,
+            yAxisID: "y",
+          },
+          {
+            label: "Coin 2",
+            data: prices2?.map((data) => data[1]),
+            borderWidth: 1,
+            fill: false,
+            tension: 0.25,
+            backgroundColor: "transparent",
+            borderColor: "#61c96f",
+            pointRadius: 0,
+            yAxisID: "y1",
+          },
+        ],
+      });
+    }
+
     setLoading(false);
   };
 
@@ -143,6 +243,7 @@ function ComparePage() {
           <div className="grey-container">
             <List coin={coinData2} />
           </div>
+          <LineChart chartData={chartData} options={options} />
           <div className="grey-container">
             <Info name={coinData1.name} desc={coinData1.desc} />
           </div>
