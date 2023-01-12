d4d506ccf9b8cf4a6913b023d4eb5f1e2cdc911c
tabrez510 <alamtabrez510@gmail.com>
2023-01-12 09:42:27 +0530

    prices and days array in chart js

--- src/pages/coin.js
+++ src/pages/coin.js
@@ -1,18 +1,35 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import LineChart from "../components/Coin/Chart";
 import Info from "../components/Coin/Info/info";
 import Header from "../components/Common/Header";
 import Loading from "../components/Common/Loading/loading";
 import List from "../components/Dashboard/ListComponent/List";
 import { getCoinData } from "../functions/getCoinData";
 import { getCoinPrices } from "../functions/getCoinPrices";
+import { getDaysArray } from "../functions/getDaysArray";
 
 function CoinPage() {
   const { id } = useParams();
   const [coin, setCoin] = useState({});
-  const [days, setDays] = useState(7);
+  const [days, setDays] = useState(90);
   const [loading, setLoading] = useState(true);
+  const today = new Date();
+  const priorDate = new Date(new Date().setDate(today.getDate() - days));
+
+  const [chartData, setChartData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [],
+        fill: false,
+        borderColor: "#fff",
+        tension: 0.1,
+      },
+    ],
+  });
 
   useEffect(() => {
     if (id) {
@@ -42,6 +59,19 @@ function CoinPage() {
     }
     if (prices) {
       console.log("Prices", prices);
+
+      setChartData({
+        labels: getDaysArray(priorDate, today),
+        datasets: [
+          {
+            label: "Prices",
+            data: prices?.map((data) => data[1]),
+            fill: false,
+            borderColor: "#fff",
+            tension: 0.1,
+          },
+        ],
+      });
     }
   };
 
@@ -55,6 +85,7 @@ function CoinPage() {
           <div className="grey-container">
             <List coin={coin} />
           </div>
+          <LineChart chartData={chartData} />
           <Info name={coin.name} desc={coin.desc} />
         </>
       )}
