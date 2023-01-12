b2005a3157e07a95b3841e9bbdf496da2c3b8600
tabrez510 <alamtabrez510@gmail.com>
2023-01-12 18:35:05 +0530

    favicon and other changes

--- src/components/LandingPage/Intro/index.js
+++ src/components/LandingPage/Intro/index.js
@@ -3,6 +3,7 @@ import Button from "../../Common/Button/Button";
 import "./styles.css";
 import { motion } from "framer-motion";
 import PhoneComponent from "./PhoneComponent";
+import { RWebShare } from "react-web-share";
 function LandingIntro() {
   return (
     <div className={"wrapper"}>
@@ -39,11 +40,21 @@ function LandingIntro() {
           transition={{ duration: 1, delay: 0.75 }}
         >
           <Button text="Dashboard" />
-          <Button text="Share" outlined={true} />
+          <RWebShare
+            data={{
+              text: "Crypto Dashboard made using React JS in 2022",
+              url: "https://crypto-dashboard-nov.netlify.app/",
+              title: "Crypto Dashboard",
+            }}
+            onClick={() => console.log("shared successfully!")}
+          >
+            <Button text="Share" outlined={true} />
+          </RWebShare>
         </motion.div>
       </div>
       <PhoneComponent />
     </div>
   );
 }
+
 export default LandingIntro;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../components/Common/Header";
 import LandingIntro from "../components/LandingPage/Intro";
 
